135a9e9a68ae68a07a2fec866922a96a6d20b918
David Robison <drbr56@gmail.com>
2022-11-16 10:26:59 -0700

    Imported Footer, Header, and Main components. Added Header, Main, and Footer components to return div.

--- src/App.js
+++ src/App.js
@@ -1,9 +1,16 @@
 import React from 'react';
+import Footer from './components/footer/Footer';
+import Header from './components/header/Header';
+import Main from './components/main/Main';
 import './styles/App.css';
 
 const App = () => {
     return (
-      <div className='App'>App</div>
+      <div className='App'>
+        <Header />
+        <Main />
+        <Footer />
+      </div>
     );
 };
 
