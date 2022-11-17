c0d879d16aa2aec7c57625018bf589b03b2cb6b0
David Robison <drbr56@gmail.com>
2022-11-17 01:52:33 -0700

    Imported Menu.css Gave return nav a className of .header_menu

--- src/components/header/menu/Menu.js
+++ src/components/header/menu/Menu.js
@@ -1,8 +1,9 @@
 import React from 'react';
+import '../../../styles/components/header/menu/Menu.css';
 
 const Menu = () => {
     return (
-        <nav>Menu</nav>
+        <nav className='header_menu'>Menu</nav>
     );
 };
 
