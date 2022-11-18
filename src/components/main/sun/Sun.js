6268d42bbb481dfb67c810344b8dc32b7a7acd6e
David Robison <drbr56@gmail.com>
2022-11-18 12:29:18 -0700

    Setup initial sun component.

--- src/components/main/sun/Sun.js
+++ src/components/main/sun/Sun.js
@@ -1,8 +1,9 @@
 import React from 'react';
+import '../../../styles/components/main/sun/Sun.css';
 
 const Sun = () => {
     return (
-        <section>Sun</section>
+        <section className='sun'>Sun</section>
     );
 };
 
