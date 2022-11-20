fc9ca9e0d2ba84f19273f03eddcfefa2c353f76e
David Robison <drbr56@gmail.com>
2022-11-20 15:47:44 -0700

    Imported AirCard.css Gave return div a className of .air_card

--- src/components/main/air/AirCard.js
+++ src/components/main/air/AirCard.js
@@ -1,8 +1,9 @@
 import React from 'react';
+import '../../../styles/components/main/air/AirCard.css';
 
 const AirCard = () => {
     return (
-        <div>AirCard</div>
+        <div className='air_card'>AirCard</div>
     );
 };
 
