f64ff67e24ca970cc1716a99f23bfd0b30d447bc
David Robison <drbr56@gmail.com>
2022-11-21 13:31:47 -0700

    Imported useState. Setup lightMode state, with an initial value of true.

--- src/App.js
+++ src/App.js
@@ -1,17 +1,19 @@
-import React from 'react';
+import React, { useState } from 'react';
 import Footer from './components/footer/Footer';
 import Header from './components/header/Header';
 import Main from './components/main/Main';
 import './styles/App.css';
 
 const App = () => {
-    return (
-      <div className='App'>
-        <Header />
-        <Main />
-        <Footer />
-      </div>
-    );
+    const [lightMode, setLightMode] = true;
+
+  return (
+    <div className='App'>
+      <Header />
+      <Main />
+      <Footer />
+    </div>
+  );
 };
 
 export default App;
