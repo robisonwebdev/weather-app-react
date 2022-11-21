558a91e2dcaf7c7f3cf970db7e22bfbc5264fd8a
David Robison <drbr56@gmail.com>
2022-11-21 13:35:11 -0700

    Imported LightModes, Menu, and SearchBar components. Destructured lightMode and setLightMode from component.

--- src/components/header/Header.js
+++ src/components/header/Header.js
@@ -1,7 +1,10 @@
 import React from 'react';
+import LightModes from './lightModes/LightModes';
+import Menu from './menu/Menu';
+import SearchBar from './search/SearchBar';
 import '../../styles/components/header/Header.css';
 
-const Header = () => {
+const Header = ({ lightMode, setLightMode }) => {
     return (
         <header>Header</header>
     );
