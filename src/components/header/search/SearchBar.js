54eeb9a00ac4e09f82fc2dbe5c690d43bad4b7b3
David Robison <drbr56@gmail.com>
2022-11-22 08:58:15 -0700

    Imported SearchIcon. Setup handleSubmit function that prevents Default for form submit.

--- src/components/header/search/SearchBar.js
+++ src/components/header/search/SearchBar.js
@@ -1,7 +1,12 @@
 import React from 'react';
+import SearchIcon from '@mui/icons-material/Search';
 import '../../../styles/components/header/search/SearchBar.css';
 
 const SearchBar = () => {
+    const handleSubmit = (event) => {
+        event.preventDefault();
+    };
+
     return (
         <section className='header_search_bar'>SearchBar</section>
     );
