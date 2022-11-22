41e4617a9c9e592bd04aac0c070b1dee9d8c6334
David Robison <drbr56@gmail.com>
2022-11-22 09:08:41 -0700

    Imported useState. Setup value state, and gave it an initial value of an empty string. Setup handleChange function to update setValue with event.target.value. Passed handleChange to input's onChange event, and passed state value to input's value property.

--- src/components/header/search/SearchBar.js
+++ src/components/header/search/SearchBar.js
@@ -1,16 +1,22 @@
-import React from 'react';
+import React, { useState } from 'react';
 import SearchIcon from '@mui/icons-material/Search';
 import '../../../styles/components/header/search/SearchBar.css';
 
 const SearchBar = () => {
+    const [value, setValue] = useState('');
+
     const handleSubmit = (event) => {
         event.preventDefault();
+    };
+
+    const handleChange = (event) => {
+        setValue(event.target.value);
     };
 
     return (
         <section className='header_search_bar'>
             <form onSubmit={handleSubmit}>
-                <input type='text' />
+                <input type='text' onChange={handleChange} value={value} />
                 <button type='submit'>
                     <SearchIcon />
                 </button>
