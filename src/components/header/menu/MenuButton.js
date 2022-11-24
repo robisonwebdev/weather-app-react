0bc27e1d6a66528e2342174ad79837323bff41e0
David Robison <drbr56@gmail.com>
2022-11-24 04:26:57 -0700

    Imported MeneIcon and added it to button.

--- src/components/header/menu/MenuButton.js
+++ src/components/header/menu/MenuButton.js
@@ -1,10 +1,13 @@
 import React from 'react';
+import MenuIcon from '@mui/icons-material/Menu';
 import '../../../styles/components/header/menu/MenuButton.css';
 
 const MenuButton = () => {
     return (
         <section className='menu_button'>
-            <button>MenuButton</button>
+            <button>
+                <MenuButton />
+            </button>
         </section>
     );
 };
