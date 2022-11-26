7349326bfda0d6c79e1539d03b37faefd0a4fa0d
David Robison <drbr56@gmail.com>
2022-11-26 15:20:15 -0700

    Setup initial fetchData callback. Setup useEffect that calls fetchData.

--- src/components/main/Main.js
+++ src/components/main/Main.js
@@ -3,6 +3,13 @@ import axios from 'axios';
 import '../../styles/components/main/Main.css';
 
 const Main = () => {
+
+    const fetchData = useCallback(() => {}, []);
+
+    useEffect(() => {
+        fetchData();
+    }, [fetchData]);
+
     return (
         <main>Main</main>
     );
