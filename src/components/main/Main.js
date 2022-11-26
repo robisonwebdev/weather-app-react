8e0e350d48cc5056f19dfc90dd3bd74b9cfaaee9
David Robison <drbr56@gmail.com>
2022-11-26 15:44:35 -0700

    Setup fetchCoordinates to take location prop and pass it to api, and save api data to coordinates state. Replaces fetchData in useEffect with fetchCoordinates.

--- src/components/main/Main.js
+++ src/components/main/Main.js
@@ -5,16 +5,30 @@ import '../../styles/components/main/Main.css';
 const Main = ({ location }) => {
     const [coordinates, setCoordinates] = useState('');
 
-    const fetchCoordinates = useCallback(() => {}, []);
+    const fetchCoordinates = useCallback(() => {
+        if (location === '') return null;
+
+        const coordinates_API = `http://api.positionstack.com/v1/forward?access_key=f2ca9cc4bfa0018f4848c0055b36aacb&query=${location}`;
+
+        axios
+        .get(coordinates_API)
+        .then(res => {
+            setCoordinates(res.data);
+        })
+        .catch(err => console.log(err));
+    }, [location]);
 
     const fetchData = useCallback(() => {}, []);
 
     useEffect(() => {
-        fetchData();
-    }, [fetchData]);
+        fetchCoordinates();
+    }, [fetchCoordinates]);
 
     return (
-        <main>Main</main>
+        <main>
+            {console.log(coordinates)}
+            Main
+        </main>
     );
 };
 
