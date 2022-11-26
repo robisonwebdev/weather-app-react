a8c50c8d428849727d4cc4ecb68c3ea897aec9bc
David Robison <drbr56@gmail.com>
2022-11-26 15:30:05 -0700

    Destrucutred location from component. Setup coordinates state, with an initial value of empty string. Setup initial fetchCoordinates callback.

--- src/components/main/Main.js
+++ src/components/main/Main.js
@@ -2,7 +2,10 @@ import React, { useCallback, useEffect, useState } from 'react';
 import axios from 'axios';
 import '../../styles/components/main/Main.css';
 
-const Main = () => {
+const Main = ({ location }) => {
+    const [coordinates, setCoordinates] = useState('');
+
+    const fetchCoordinates = useCallback(() => {}, []);
 
     const fetchData = useCallback(() => {}, []);
 
