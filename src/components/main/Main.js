0604a69086a6c9ff931397a3220b0a2d5e033aa7
David Robison <drbr56@gmail.com>
2022-11-26 15:18:53 -0700

    Imported useCallback, useEffect, useState, and axios.

--- src/components/main/Main.js
+++ src/components/main/Main.js
@@ -1,4 +1,5 @@
-import React from 'react';
+import React, { useCallback, useEffect, useState } from 'react';
+import axios from 'axios';
 import '../../styles/components/main/Main.css';
 
 const Main = () => {
