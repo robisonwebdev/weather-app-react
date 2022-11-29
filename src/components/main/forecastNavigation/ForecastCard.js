479f17f7e72642eaf55563abbb2bac936be00f40
David Robison <drbr56@gmail.com>
2022-11-29 09:54:50 -0700

    Setup empty getWeatherIcon function, that will be used to return weather icon. Called getWeatherIcon in .card_weather

--- src/components/main/forecastNavigation/ForecastCard.js
+++ src/components/main/forecastNavigation/ForecastCard.js
@@ -4,12 +4,16 @@ import '../../../styles/components/main/forecastNavigation/ForecastCard.css';
 const ForecastCard = () => {
     const getForecastDay = () => {};
 
+    const getWeatherIcon = () => {};
+
     return (
         <div className='forecast_card'>
             <div className='card_day'>
                 <h3>{getForecastDay()}</h3>
             </div>
-            <div className='card_weather'></div>
+            <div className='card_weather'>
+                {getWeatherIcon()}
+            </div>
         </div>
     );
 };
